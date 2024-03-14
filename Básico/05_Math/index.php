fd9039f203c91ed050afe4314e22dcdeba1f386f
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-03-14 19:56:04 -0300

    'Biblioteca' Math

---
+++ Básico/05_Math/index.php
@@ -0,0 +1,16 @@
+<?php 
+echo pi();
+
+$numbers = [0, 150, 30, 20, -8, -200];
+echo min($numbers);
+echo max($numbers);
+
+echo abs(-78);
+
+echo sqrt(36);
+
+echo round(0.6);
+echo round(0.49);
+
+echo rand(1, 100);
+?>
