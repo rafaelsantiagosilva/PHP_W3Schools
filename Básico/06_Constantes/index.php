49c20a2968452f217d1f8db4ea630912199e87b8
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-03-14 20:12:56 -0300

    Constantes

---
+++ Básico/06_Constantes/index.php
@@ -0,0 +1,8 @@
+<?php
+// define(NOME_DA_CONSTANTE, valor)
+define("GREETING", "Welcome to W3Schools.com"); // Funciona em qualquer lugar
+echo GREETING;
+
+// const
+const MY_CONSTANT = "Constante"; // Não pode ser declarada dentro de um bloco, como IF ou uma FUNÇÃO
+?>
